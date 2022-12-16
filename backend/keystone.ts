e6be0e9946120d34798bfa7171b9a32c275d23fd
Marknjo <marknjo7@gmail.com>
2022-12-16 10:00:50 +0300

    ci(backend): Keystone Server option

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -3,10 +3,24 @@ import { config } from '@keystone-6/core';
 
 import { dbUrl } from './lib/buildDbUrl';
 
+const frontEndUrl = process.env.FRONTEND_URL;
+if (!frontEndUrl) {
+  throw new Error(
+    'CONFIGURATION ERROR: Must Provide a FRONTEND_URL environmental variable',
+  );
+}
+
 export default config({
+  server: {
+    cors: {
+      origin: [frontEndUrl],
+      credentials: true,
+    },
+  },
   db: {
     provider: 'postgresql',
     url: dbUrl(),
+    // @TODO: Add data seeding here
   },
   lists: {},
 });
