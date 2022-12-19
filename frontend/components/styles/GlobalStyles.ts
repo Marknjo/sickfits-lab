8da6e6ba2216a91d80bb17a4810822fb8c1baa5d
Marknjo <marknjo7@gmail.com>
2022-12-19 11:16:57 +0300

    ci(frontend): apollo graphql ssr & install dependencies

--- frontend/components/styles/GlobalStyles.ts
+++ frontend/components/styles/GlobalStyles.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
   @font-face {
@@ -58,4 +58,4 @@ export const GlobalStyles = createGlobalStyle`
   }
 
 
-`;
+`
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,17 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
 import nProgress from 'nprogress'
 import { Router } from 'next/router'
+
+/// Local
 import Page from '../components/layouts/Page'
 import '../components/styles/nprogress.css'
+import { useApollo } from '../lib/apolloClient'
 
 Router.events.on('routeChangeStart', () => nProgress.start())
 Router.events.on('routeChangeComplete', () => nProgress.done())
 Router.events.on('routeChangeError', () => nProgress.done())
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps)
+
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apolloClient}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   )
 }
