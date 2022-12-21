ac1e78f88f604d7d91e338a0bd019b84109e4274
Marknjo <marknjo7@gmail.com>
2022-12-21 14:21:02 +0300

    ci(frontend): add update product barebones (component/page)

---
+++ frontend/components/products/UpdateProduct.tsx
@@ -0,0 +1,9 @@
+function UpdateProduct() {
+  return (
+    <>
+      <h2>Update Product Page</h2>
+    </>
+  )
+}
+
+export default UpdateProduct
---
+++ frontend/pages/update.tsx
@@ -0,0 +1,11 @@
+import UpdateProduct from '../components/products/UpdateProduct'
+
+function UpdateProductPage() {
+  return (
+    <main>
+      <UpdateProduct />
+    </main>
+  )
+}
+
+export default UpdateProductPage
